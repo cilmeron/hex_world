408ebbea91555b2536eb27f53a808d43d1ea699c
PIROTESS\david <cilmeron@gmail.com>
2023-04-15 15:58:38 +0200

    Camera, Selection fixed

--- Scripts/Camera/selected_dictionary.cs
+++ Scripts/Camera/selected_dictionary.cs
@@ -17,7 +17,19 @@ public class selected_dictionary : MonoBehaviour
             Debug.Log("Added " + id + " to selected dict");
         }
     }
-
+    public void select(Entity entity)
+    {
+        int id = entity.gameObject.GetInstanceID();
+        if (selectedTable.ContainsKey(id))
+        {
+            removeAllBut(id);
+        }
+        else
+        {
+            deselectAll();
+            addSelected(entity);
+        }
+    }
     public void removeSelected(Entity entity)
     {
         int id = entity.gameObject.GetInstanceID();
@@ -29,33 +41,28 @@ public class selected_dictionary : MonoBehaviour
             Debug.Log("Removed " + id + " from selected dict");
         }
     }
-
-    public void deselect(int id)
+    private void removeAllBut(int id)
     {
-        Destroy(selectedTable[id].GetComponent<selection_component>());
-        selectedTable.Remove(id);
+        foreach(KeyValuePair<int,Entity> pair in selectedTable)
+        {
+            if(pair.Value != null && pair.Key != id)
+            {
+                Destroy(selectedTable[pair.Key].gameObject.GetComponent<selection_component>());
+                selectedTable.Remove(pair.Key);
+                Debug.Log("Removed " + pair.Key + " from selected dict");
+            }
+        }
     }
-
     public void deselectAll()
     {
         foreach(KeyValuePair<int,Entity> pair in selectedTable)
         {
             if(pair.Value != null)
             {
-                Destroy(selectedTable[pair.Key].GetComponent<selection_component>());
+                Destroy(selectedTable[pair.Key].gameObject.GetComponent<selection_component>());
+                Debug.Log("Removed " + pair.Key + " from selected dict");
             }
         }
         selectedTable.Clear();
-    }
-    public void selectAll()
-    {
-        foreach(KeyValuePair<int,Entity> pair in selectedTable)
-        {
-            if (pair.Value != null)
-            {
-                selectedTable.Add(pair.Key, pair.Value);
-                pair.Value.gameObject.AddComponent<selection_component>();
-            }
-        }
     }
 }
