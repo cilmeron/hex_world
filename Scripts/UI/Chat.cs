28d161ce57f5d0379f29ea1554911f1263cd451d
PIROTESS\david <cilmeron@gmail.com>
2023-04-18 10:42:46 +0200

    Network Manager and Chat System

---
+++ Scripts/UI/Chat.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chat : MonoBehaviour
+{
+    NetworkManager networkManager = new NetworkManager();
+    public TMPro.TMP_Text ingamechat;
+    public ScrollRect scrollRect;
+    public int index = 0;
+    List<string> chathistory = new List<string>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<TMPro.TMP_InputField>().Select();
+        this.GetComponent<TMPro.TMP_InputField>().ActivateInputField();
+        ingamechat.text = networkManager.ingamechat;     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<TMPro.TMP_InputField>().Select();
+        this.GetComponent<TMPro.TMP_InputField>().ActivateInputField();
+        ingamechat.text = networkManager.ingamechat;
+        
+
+        if (Input.GetButtonUp("Submit"))
+        {
+            string text = this.GetComponent<TMPro.TMP_InputField>().text;
+            chathistory.Insert(0, text);
+            if (chathistory.Count >= 50)
+            {
+                chathistory.RemoveAt(chathistory.Count-1);
+            }
+            //let's escape the pipe and colon symbols - everything else should be fine
+            string escaped = this.GetComponent<TMPro.TMP_InputField>().text;
+            escaped = escaped.Replace("|", ";;;pipesymbol;;;").Replace(":", ";;;colon;;;");
+            networkManager.SendMsg("T:"+escaped+":\n");
+            this.GetComponent<TMPro.TMP_InputField>().text = "";
+            index = 0;
+            scrollRect.verticalScrollbar.value = 0;
+            //Send chat
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.GetComponent<TMPro.TMP_InputField>().text = chathistory[index];
+            index++;
+            if (index > chathistory.Count-1)
+                index = chathistory.Count-1;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            index--;
+            if (index < 0)
+            {
+                this.GetComponent<TMPro.TMP_InputField>().text = "";
+                index = 0;
+            }
+            else
+            {
+                this.GetComponent<TMPro.TMP_InputField>().text = chathistory[index];
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            this.GetComponent<TMPro.TMP_InputField>().text = "";
+          //  World.Instance.chatactive = false;
+          //  World.Instance.inUI = false;
+          //  player.disablechat = true;
+            index = 0;
+        }
+
+    }
+}
