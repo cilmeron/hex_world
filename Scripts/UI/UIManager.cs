72f4cd4cf095062756b8579ebe3c5088f5bb4411
Alexander Puchinger <puchingeralexander@gmail.com>
2023-04-21 17:59:01 +0200

    EntityUI is now filled with respect of IEntityUI Interface' data

--- Scripts/UI/UIManager.cs
+++ Scripts/UI/UIManager.cs
@@ -1,17 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour{
+    // Static instance variable
+    public static UIManager Instance { get; private set; }
+
+    // Private constructor
+    private UIManager() { }
+
     private Player player;
     [SerializeField] private UITopBar uiTopbar;
     [SerializeField] private UIEntity uiEntity;
 
-    [SerializeField] private Entity _entity;
+    [SerializeField] private Entity selectedEntity;
+
+    [SerializeField] private CanvasScaler canvasScaler;
+    
+    private SelectionManager selectionManager;
+
+    // Awake is called when the script instance is being loaded
+    private void Awake(){
+        // Check if instance already exists
+        if (Instance == null){
+            // If not, set instance to this
+            Instance = this;
+        } else if (Instance != this){
+            // If instance already exists and it's not this, destroy this
+            Destroy(gameObject);
+            return;
+        }
+
+        // Set this object to not be destroyed when loading a new scene
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
+        ScaleUI();
+        selectionManager = SelectionManager.Instance;
+    }
+
+    private void ScaleUI(){
         // Set the reference resolution
         canvasScaler.referenceResolution = new Vector2(1080f, 1920f);
 
@@ -25,18 +57,26 @@ public class UIManager : MonoBehaviour{
 
     // Update is called once per frame
     void Update(){
+        int selectionCount = selectionManager.selectedDictionary.selectedTable.Values.Count;
+        if (selectionCount == 0){
+            uiEntity.ResetEntityUI();
+            uiEntity.gameObject.SetActive(false);
+        }else if ( selectionCount == 1){
+            selectedEntity = selectionManager.selectedDictionary.selectedTable.Values.First();
+            uiEntity.gameObject.SetActive(true);
+            uiEntity.SetEntityUI(selectedEntity);
+        }else{
+            //Multiple Selection   
+        }
         player = GameManager.Instance.player;
         uiTopbar.Nodes[0].SetText(GameResourceManager.GetResourceAmount(player,GameResourceManager.ResourceType.Gold).ToString());
         uiTopbar.Nodes[1].SetText(player.CalculateUnits().ToString());
         uiTopbar.Nodes[2].SetText(player.CalculateBuildings().ToString());
         uiTopbar.Nodes[3].SetText(player.Formations.Count.ToString());
-        if (_entity != null){
-            uiEntity.SetEntityUI(_entity);
-        }
     }
     
-    [SerializeField] private CanvasScaler canvasScaler;
-
-   
-    
+    public Entity SelectedEntity{
+        get => selectedEntity;
+        set => selectedEntity = value;
+    }
 }
