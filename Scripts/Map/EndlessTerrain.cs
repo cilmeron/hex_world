be30f252eb632603e8db1766b1162ac03e62621b
Gerald Hirsch <gerald.hirsch@stappone.com>
2023-05-07 17:17:45 +0200

    collision mesh generated

--- Scripts/Map/EndlessTerrain.cs
+++ Scripts/Map/EndlessTerrain.cs
@@ -85,6 +85,7 @@ public class EndlessTerrain : MonoBehaviour
 
         MeshRenderer meshRenderer;
         MeshFilter meshFilter;
+        MeshCollider meshCollider;
 
         LODInfo[] detailLevels;
         LODMesh[] lodMeshes;
@@ -104,6 +105,8 @@ public class EndlessTerrain : MonoBehaviour
             meshObject = new GameObject("Terrain Chunk");
             meshRenderer = meshObject.AddComponent<MeshRenderer>();
             meshFilter = meshObject.AddComponent<MeshFilter>();
+            meshCollider = meshObject.AddComponent<MeshCollider>();
+
             meshRenderer.material = material;
 
             meshObject.transform.position = positionV3;
@@ -162,6 +165,7 @@ public class EndlessTerrain : MonoBehaviour
                         {
                             previousLODIndex = lodIndex;
                             meshFilter.mesh = lodMesh.mesh;
+                            meshCollider.sharedMesh = lodMesh.mesh;
                         }
                         else if (!lodMesh.hasRequestedMesh)
                         {
