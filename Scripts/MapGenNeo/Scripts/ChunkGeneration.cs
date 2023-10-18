4ded2f68e39efe20ffa69107c7c0cd996d4a733a
Gerald Hirsch <gerald.hirsch@stappone.com>
2023-10-18 19:59:34 +0200

    in editor map generation works

--- Scripts/MapGenNeo/Scripts/ChunkGeneration.cs
+++ Scripts/MapGenNeo/Scripts/ChunkGeneration.cs
@@ -14,6 +14,16 @@ public class ChunkGeneration : MonoBehaviour
     public float treeRandomness;
     public float treeThreshold;
 
+    public Transform player;
+    public int chunksChunkLoaded;
+
+    public float noiseScale;
+
+    public int octaves;
+    [Range(0, 1)]
+    public float persistance;
+    public float lacunarity;
+
     public int seed;
 
     private void Start()
@@ -21,12 +31,12 @@ public class ChunkGeneration : MonoBehaviour
         waterLevel = Mathf.PerlinNoise(seed, seed) * 256;
         StartCoroutine(GenerateChunks());
         GameObject current = Instantiate(water, new Vector3((128 * chunks.x) / 2, waterLevel, (128 * chunks.y) / 2), Quaternion.identity);
-        current.transform.localScale = new Vector3(512, 512, 512);
+        current.transform.localScale = new Vector3(16 * chunks.x, 128, 16 * chunks.y);
     }
 
     public IEnumerator GenerateChunks()
     {
-        for (int x = 0; x < chunks.x; x++)
+        for (int i = 0, x = 0; x < chunks.x; x++)
         {
             for (int z = 0; z < chunks.y; z++)
             {
@@ -39,7 +49,32 @@ public class ChunkGeneration : MonoBehaviour
                 current.transform.position = new Vector3(x * (chunkResolution.x) * (128 / chunkResolution.x),
                     0f,
                     z * (chunkResolution.y) * (128 / chunkResolution.y));
-                yield return new WaitForSeconds(Time.deltaTime);
+                i++;
+                if (i == chunksChunkLoaded)
+                {
+                    i = 0;
+                    yield return new WaitForSeconds(Time.deltaTime);
+                }
+            }
+        }
+    }
+
+    public void GenerateChunksEditor()
+    {
+        for (int i = 0, x = 0; x < chunks.x; x++)
+        {
+            for (int z = 0; z < chunks.y; z++)
+            {
+                GameObject current = new GameObject("Terrain" + new Vector2(x * 128, z * 128),
+                    typeof(TerrainGenerator),
+                    typeof(MeshRenderer),
+                    typeof(MeshCollider),
+                    typeof(MeshFilter));
+                current.transform.parent = transform;
+                current.transform.position = new Vector3(x * (chunkResolution.x) * (128 / chunkResolution.x),
+                    0f,
+                    z * (chunkResolution.y) * (128 / chunkResolution.y));
+                i++;
             }
         }
     }
