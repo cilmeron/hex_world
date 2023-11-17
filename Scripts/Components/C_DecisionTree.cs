83e2d742f8b5215ae203f1f7612cd6484bb595f4
Gerald Hirsch <gerald.hirsch@stappone.com>
2023-11-17 13:24:45 +0100

    added distances, range unit info, multpl. weights. Added new SetTarget (quick fix). BUG: sometimes units do not fight, because they are out of range, but have the target set

--- Scripts/Components/C_DecisionTree.cs
+++ Scripts/Components/C_DecisionTree.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class DecisionTree
 {
-    private int distanceWeight = 1;
-    private int healthWeight = 1;
-    private int persistWeigth = 1;
+    private float distanceWeight = 1;
+    private float healthWeight = 1;
+    private float persistWeigth = 1;
+    private float rangedUnitThreshold = 10;
 
     public C_Health ChooseTarget(List<C_Health> targets, Entity selfEntity)
     {
@@ -35,23 +37,57 @@ public class DecisionTree
         // get enemy targets (ingore friendlies)
         // get distances between selfEntity and target entities
         // get health values
-        // combine health and distance -> rank this value
+        // combine health, swap and distance -> rank this value
         // return target with highest score
 
+        // select enemies
+        float rangedWeight = 1.0f;
         List<Entity> enemyTargets = entities
          .Where(entity => entity.GetPlayer() != selfEntity.GetPlayer() && selfEntity.GetPlayer() != null && entity.CHealth != null)
          .ToList();
 
-        // float distance = Vector3.Distance(entities[0].transform.localPosition, selfEntity.transform.localPosition);
-        List<double> distances = new List<double>();
-        foreach (Entity target in enemyTargets)
+        if (enemyTargets.Count == 0) return null;
+        // calc distances to enemies
+        var distances = enemyTargets.Select(entity => Vector3.Distance(selfEntity.transform.localPosition, entity.transform.localPosition)).ToArray();
+        float maxDistance = distances.Max();
+
+        var normalizedDistances = distances.Select(distance => distance / maxDistance).ToArray(); // maximu distance will be 1
+
+        int attackRange = selfEntity.CCombat.GetAttackRange();
+        if (attackRange >= rangedUnitThreshold) // just an additional idea to weigh in the unit type. If it is a ranged unit -> weigh the distance less
         {
-            // get distances between self and enemys
+            rangedWeight = 0.5f;
         }
 
-        // calc score with healths
+        int i = 0;
+        int swap = 0;
+        float relativeHP;
+        float reversedDistance;
+        float targetValue = 0f;
+        List<float> costList = new List<float>();
 
-        enemyTargets.Sort((a, b) => a.CHealth.CurrentHP.CompareTo(b.CHealth.CurrentHP));
-        return enemyTargets.Count > 0 ? enemyTargets[0].CHealth : null;
+        foreach (Entity target in enemyTargets)
+        {
+            // three metrics are calculated which are used to estimat the target value. They are scaled from 0 (worst) to 1 (best)
+            swap = (target == oldTarget || oldTarget != null) ? 1 : 0;
+            relativeHP = 1 - (target.CHealth.CurrentHP / target.CHealth.GetMaxHp());
+            reversedDistance = 1 - normalizedDistances[i];
+
+            targetValue = (swap * persistWeigth + relativeHP * healthWeight + reversedDistance * (distanceWeight * rangedWeight)) /
+                (persistWeigth + healthWeight + (distanceWeight * rangedWeight));
+            // ensure that the targetValue again ranges from 0 to 1. The weights determine the weight of the individual metrics
+            costList.Add(targetValue);
+            i++;
+        }
+
+        if (costList.Count > 0)
+        {
+            int bestTarget = costList.IndexOf(costList.Max());
+            return enemyTargets[bestTarget].CHealth;
+        }
+        else
+        {
+            return null;
+        }
     }
 }
